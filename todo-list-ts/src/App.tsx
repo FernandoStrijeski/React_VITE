b1cf807104ca681c0bfa7c234d8b788f869ca4dd
Fernando Ricardo Strijeski <fernando.strijeski@linx.com.br>
2024-04-12 22:58:33 -0300

    Finalizado componente Task com as funções de toggle e remove e interface;

--- todo-list-ts/src/App.tsx
+++ todo-list-ts/src/App.tsx
@@ -5,11 +5,45 @@ import { Task } from "./components/Task";
 import { CreateTaskModal } from "./components/CreateTaskModal";
 import Modal from "react-modal";
 import { useState } from "react";
+import { ITaskProps } from "./components/Task/types";
 
 Modal.setAppElement("#root");
 
 function App() {
+  const [tasks, setTasks] = useState<ITaskProps[]>(() => {
+    const taskFromLocalStorage = localStorage.getItem("tasks");
+
+    if (!taskFromLocalStorage) {
+      return [];
+    }
+
+    return JSON.parse(taskFromLocalStorage);
+  });
+
   const [isCreatedTaskModalOpen, setIsCreatedTaskModalOpen] = useState(false);
+
+  function handleToggleTask(clickedTask: ITaskProps) {
+    setTasks((prevState) => {
+      return prevState.map((task) => {
+        if (task.id === clickedTask.id) {
+          return {
+            ...task,
+            isCompleted: !task.isCompleted,
+          };
+        }
+
+        return task;
+      });
+    });
+  }
+
+  function handleRemoveTask(taskId: number) {
+    setTasks((prevState) => {
+      return prevState.filter((task) => {
+        return task.id !== taskId;
+      });
+    });
+  }
 
   function handleRequestCloseCreatedTaskModal() {
     setIsCreatedTaskModalOpen(false);
@@ -31,7 +65,12 @@ function App() {
         </header>
 
         <div className={styles.tasks}>
-          <Task />
+          {tasks.map((task) => (
+            <Task key={task.id}
+            task={task}
+            handleToggleTask={handleToggleTask}
+            handleRemoveTask={handleRemoveTask} />
+          ))}
         </div>
       </section>
 
--- todo-list-ts/src/components/Task/index.tsx
+++ todo-list-ts/src/components/Task/index.tsx
@@ -1,17 +1,20 @@
 import { Trash } from "react-feather";
 import styles from "./styles.module.css";
+import { IComponentTaskProps } from "./types";
 
-export function Task({ task, handleToggleTask, handleRemoveTask }) {
+export function Task({ task, handleToggleTask, handleRemoveTask } : IComponentTaskProps) {
   return (
     <div className={styles.container}>
       <input
+        readOnly
+        checked={task.isCompleted}
         type="checkbox"
         className={styles.input}
         onClick={() => {
           handleToggleTask(task);
         }}
       />
-      <label>task.title</label>
+      <label className={task.isCompleted ? styles.completed : ''}>task.title</label>
       <div className={styles.buttonsWrapper}>
         <button
           type="button"
